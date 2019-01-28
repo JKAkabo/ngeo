1991cdeb85dd77ea572bca93df1491904561b66d
John <JKAkabo@github.com>
2019-01-28 12:25:51 +0000

    added login feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login as lin
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def login(request):
+    template_name = 'accounts/login.html'
+    login_form = LoginForm
+
+    if request.method == 'POST':
+        login_form = login_form(data=request.POST)
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    lin(request, user)
+                    messages.success(request, f'Successfully logged in as {user.username}', 'alert-success')
+                    return redirect('product:index')
+                else:
+                    messages.success(request, 'Your account is inactive. Please contact your system administrator.')
+            else:
+                messages.error(request, 'Incorrect username or password. Please try again.', 'alert-danger')
+                context = {
+                    'login_form': login_form,
+                }
+                return render(request, template_name, context)
+        else:
+            messages.error(request, 'The form contains some errors. Please try again.' 'alert-danger')
+            context = {
+                'login_form': login_form,
+            }
+            return render(request, template_name, context)
+    else:
+        context = {
+            'login_form': login_form,
+        }
+        return render(request, template_name, context)
--- product/views.py
+++ product/views.py
@@ -2,5 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index(reqeust):
-    return HttpResponse('hello world')
+def index(request):
+    template_name = 'product/index.html'
+    return render(request, template_name)
