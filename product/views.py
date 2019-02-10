599f15b028682b1d52f8e6254b2caa46a8888aaa
John <JKAkabo@github.com>
2019-02-10 21:26:54 +0000

    added docstrings for django docs

--- product/views.py
+++ product/views.py
@@ -6,11 +6,27 @@ from .models import Stock
 
 
 def index(request):
+    """
+    Displays the total total stock and stock value from the model :model:`product.Stock`.
+
+    **Template**
+    :template:`product/index.html`
+    """
     template_name = 'product/index.html'
     return render(request, template_name)
 
 
 def add_product(request):
+    """
+    Displays a form that allows the addition of a product
+
+    **Context**
+        `` **GET** ``
+        :form: A Form class for adding products
+    
+    **Template**
+    :template:`product/add_product.html`
+    """
     template_name = 'product/add_product.html'
     form = AddProductForm
 
@@ -27,6 +43,16 @@ def add_product(request):
 
 
 def edit_unit_price(request, stock_id):
+    """
+    Displays a form that allows users to edit the the unit price of a product
+
+    **Context**
+        `` **GET** ``
+        :form: A Form class for editting a the unit price of a product
+
+    **Template**
+    :template:`product/edit_unit_price.html`
+    """
     template_name = 'product/edit_unit_price.html'
     stock = Stock.objects.get(id=stock_id)
     form = EditUnitPriceForm
@@ -43,6 +69,16 @@ def edit_unit_price(request, stock_id):
         return render(request, template_name, context)
 
 def add_to_stock(request, stock_id):
+    """
+    Displays a form that allows user to add to a a product's stock
+
+    **Context**
+        `` **GET** ``
+        :form: A Form class for adding to stock
+
+    **Template**
+    :template:`product/add_to_stock.html`
+    """
     template_name = 'product/add_to_stock.html'
     stock = Stock.objects.get(id=stock_id)
     form = AddToStockForm
